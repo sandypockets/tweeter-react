739092c9d08207bf2744450de4b9d2ab5166a8e6
sandypockets <zac@znacreative.com>
2021-04-11 17:38:46 -0400

    Add props to Tweet component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ import { TweetForm } from "./components/TweetForm";
 import { Tweet } from "./components/Tweet";
 
 
-
+const tweets = [
+  {
+    name: "Michael Scott",
+    handle: "@greatscott",
+    avatar: "https://i.imgur.com/2WZt0D6.png",
+    text: "You miss 100% of the shots you don't take",
+    date: "10 days ago"
+  },
+  {
+    name: "Andy Bernard",
+    handle: "@herecomestrebel",
+    avatar: "https://i.imgur.com/2WZt0D6.png",
+    text: "Do not test my politeness",
+    date: "12 days ago"
+  }
+]
 
 
 function App() {
@@ -14,10 +29,14 @@ function App() {
       <div className={'page-container'}>
       <Navigation />
       <Profile />
-      <div className={'container'}>
+      <main className={'container'}>
         <TweetForm />
-      <Tweet />
-      </div>
+        <section className={'tweets'}>
+          <Tweet name={tweets[0].name} handle={tweets[0].handle} text={tweets[0].text} date={tweets[0].date}/>
+          {/* Adding second tweet for styling/debugging */}
+          <Tweet name={tweets[1].name} handle={tweets[1].handle} text={tweets[1].text} date={tweets[1].date}/>
+        </section>
+      </main>
       </div>
     </div>
   );
