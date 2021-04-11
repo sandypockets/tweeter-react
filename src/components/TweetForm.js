25816447531ae6ca8554f8c9db4c7649c1a6fdb2
sandypockets <zac@znacreative.com>
2021-04-11 19:19:12 -0400

    Set up character counter

--- src/components/TweetForm.js
+++ src/components/TweetForm.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TweetForm.css';
 
 export const TweetForm = () => {
+  const [tweetText, setTweetText] = useState("")
+  const tweetRemainingLength = 140 - tweetText.length;
+  const outputStyle = {color: tweetRemainingLength >= 0 ? 'black' : 'red'};
 
+ 
   return(
     <section id="write-new-tweet" className="new-tweet">
       <h2>Compose Tweet</h2>
@@ -10,10 +14,10 @@ export const TweetForm = () => {
         <form className="tweet-form" method="POST" action="/tweets">
           <label htmlFor="tweet-text">
           </label>
-          <textarea name="text" id="tweet-text" placeholder="What are you humming about?" />
+          <textarea name="text" id="tweet-text" placeholder="What are you humming about?" value={tweetText} onChange={event => setTweetText(event.target.value)} />
           <div>
             <button className="new-tweet-btn" type="submit">Tweet</button>
-            <output name="counter" className="counter" htmlFor="tweet-text">140</output>
+            <output style={outputStyle} name="counter" className="counter" htmlFor="tweet-text">{140 - tweetText.length}</output>
           </div>
         </form>
       </div>
